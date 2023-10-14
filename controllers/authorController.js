f7444c73db4b1f78fd9528e08f4759720ce98c88
LC <gingeraleisthebest@gmail.com>
2023-10-14 19:30:18 -0400

    added details page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,5 @@
 const Author = require("../models/author");
+const Book = require("../models/book");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all Authors.
@@ -13,7 +14,21 @@ exports.author_list = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific Author.
 exports.author_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMETED: Author detail: ${req.params.id}`);
+  const [author, allBooksByAuthor] = await Promise.all([
+    Author.findById(req.params.id).exec(),
+    Book.find({ author: req.params.id }, 'title summary').exec(),
+  ]);
+
+  if (author === null) {
+    const err = new Error('Author not found');
+    err.status = 404;
+    return next(err);
+  }
+  res.render('author_detail', {
+    title: 'Author Detail',
+    author,
+    author_books: allBooksByAuthor,
+  })
 });
 
 // Display Author create form on GET.
