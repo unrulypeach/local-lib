e72b3920d1d9f255667dee7b4dd4e82253a84d34
LC <gingeraleisthebest@gmail.com>
2023-10-14 19:31:58 -0400

    added missing /

--- routes/catalog.js
+++ routes/catalog.js
@@ -17,22 +17,22 @@ router.get('/', book_controller.index);
 router.get('/book/create', book_controller.book_create_get);
 
 // POST request for creating Book.
-router.post('book/create', book_controller.book_create_post);
+router.post('/book/create', book_controller.book_create_post);
 
 // GET request to delete Book.
-router.get('book/:id/delete', book_controller.book_delete_get);
+router.get('/book/:id/delete', book_controller.book_delete_get);
 
 // POST request to delete Book.
-router.post('book/:id/delete', book_controller.book_delete_post);
+router.post('/book/:id/delete', book_controller.book_delete_post);
 
 // GET request to update Book.
-router.get('book/:id/update', book_controller.book_update_get);
+router.get('/book/:id/update', book_controller.book_update_get);
 
 // POST request to update Book.
-router.post('book/:id/update', book_controller.book_update_post);
+router.post('/book/:id/update', book_controller.book_update_post);
 
 // GET request for one Book.
-router.get('book/:id', book_controller.book_detail);
+router.get('/book/:id', book_controller.book_detail);
 
 // GET request for list of all Book items.
 router.get('/books', book_controller.book_list);
