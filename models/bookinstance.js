de500e7f55ad754fb3f1012d579f43b56c37879f
LC <gingeraleisthebest@gmail.com>
2023-10-14 19:31:29 -0400

    added formatted date to virtual

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -21,6 +21,6 @@ BookInstanceSchema.virtual('url').get(function(){
 
 BookInstanceSchema.virtual("due_back_formatted").get(function () {
   return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
-})
+});
 
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
